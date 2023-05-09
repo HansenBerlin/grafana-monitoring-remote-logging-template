4778d0eeb41d6c3578dbee3ad86f502eae64b534
Hannes Roever <h.roever@zertificon.com>
2023-05-09 15:16:09 +0200

    init

---
+++ RemoteLoggingBlazorExample/MetricExamples/Monitor.cs
@@ -0,0 +1,34 @@
+﻿using Prometheus;
+
+namespace RemoteLoggingBlazorExample.MetricExamples;
+
+public static class Monitor
+{
+    public static readonly Counter ProcessedJobCount = Metrics
+        .CreateCounter("jobs_processed_total", "Number of processed jobs.");
+    
+    public static readonly Gauge JobsInQueue = Metrics
+        .CreateGauge("jobs_queued", "Number of jobs waiting for processing in the queue.");
+    
+    public static readonly Histogram OrderValueHistogram = Metrics
+        .CreateHistogram("order_value_usd", "Histogram of received order values (in USD).",
+            new HistogramConfiguration
+            {
+                Buckets = Histogram.LinearBuckets(start: 100, width: 100, count: 10)
+            });
+    
+    public static readonly Summary RequestSizeSummary = Metrics
+        .CreateSummary("request_size_bytes", "Summary of request sizes (in bytes) over last 10 minutes.",
+            new SummaryConfiguration
+            {
+                Objectives = new[]
+                {
+                    new QuantileEpsilonPair(0.5, 0.05),
+                    new QuantileEpsilonPair(0.9, 0.05),
+                    new QuantileEpsilonPair(0.95, 0.01),
+                    new QuantileEpsilonPair(0.99, 0.005),
+                }
+            });
+
+
+}
--- RemoteLoggingBlazorExample/Program.cs
+++ RemoteLoggingBlazorExample/Program.cs
@@ -1,9 +1,5 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.Extensions.Logging.Configuration;
 using Prometheus;
 using Serilog;
-using Serilog.Sinks.Grafana.Loki;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,7 +14,6 @@ var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 app.UseMetricServer();
